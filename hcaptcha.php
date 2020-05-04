6c9828f3b6000c995bae5fbae7575c622991f25e
RikoDEV <18230443+RikoDEV@users.noreply.github.com>
2020-05-04 10:09:49 +0200

    First release

---
+++ hcaptcha.php
@@ -0,0 +1,22 @@
+<?php
+
+$siteKey = yourls_get_option( 'rikodev_hcaptcha_site' );
+$secret = yourls_get_option( 'rikodev_hcaptcha_private' );
+
+// The response from reCAPTCHA
+$resp = false;
+
+if ($siteKey == "" || $secret == "") {
+
+    die("To use hCaptcha you must get an API key.");
+
+} elseif (isset($_POST['h-captcha-response']) && !empty($_POST['h-captcha-response'])) {
+
+    $verify = file_get_contents('https://hcaptcha.com/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response'].'&remoteip='.$_SERVER['REMOTE_ADDR']);
+    $data = json_decode($verify);
+
+    if ($data->success) $resp = true;
+
+}
+
+?>
